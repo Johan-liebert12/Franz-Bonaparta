586a7ce56659ac386e8b21bf5b5218c0c987088f
Johan-liebert12 <k7688226@gmail.com>
2026-05-05 01:52:52 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,29 @@
+from flask import Flask, request, send_file
+import yt_dlp
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return open("index.html").read()
+
+@app.route('/download', methods=['POST'])
+def download():
+    url = request.form['url']
+    
+    ydl_opts = {
+    'format': 'bestvideo+bestaudio/best',
+    'merge_output_format': 'mp4',
+    'outtmpl': 'video.%(ext)s'
+   }
+
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+    for file in os.listdir():
+        if file.startswith("video"):
+            return send_file(file, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
